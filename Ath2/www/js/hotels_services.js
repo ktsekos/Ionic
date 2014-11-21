fda04c57840dc6913ceac314751c76362849fe03
ktsekos <ktsekos@athinorama.gr>
2014-11-21 15:21:38 +0200

    pel

--- Ath2/www/js/hotels_services.js
+++ Ath2/www/js/hotels_services.js
@@ -8,7 +8,7 @@ angular.module('starter.hotels_services', [])
             'async': false,
             'global': false,
             'dataType': "json",
-            'url': "./data/HotelListAll.json",
+            'url': "./data/Peloponnisos.json",
             'success': function (data) {
                 //console.log('all good');
                 //str = data;
